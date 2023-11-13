8f8d322478b9f8ecccbd076793954121a27dd42d
junhowoo <junho.woo@musinsa.com>
2023-11-13 20:39:13 +0900

    feat: [2nd-readitems] todolist 프로그램에서 할일 목록 응답하는 router 작성
    - /todo-items 를 통해 할일 목록 응답하도록 작성

--- homework/junhowoo/backend/app.js
+++ homework/junhowoo/backend/app.js
@@ -1,12 +1,17 @@
 // express framework 를 불러온다.
 const express = require('express')
+// FE 자바스크립트에서 호출시 origin (요청 도메인) 이 달라도 요청 허용 해주는 라이브러리.
+const cors = require('cors')
+const items = require("./dummy/todo-items.json")
+
 // express 를 통해 웹 서버(웹 애플리케이션을 할당한다)
 const app = express()
 // 프로그램의 port : 3000번을 할당한다.
 const port = 3000
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/todo-items", (req, res) => {
+    res.send(items)
 })
 
 // 프로그램이 3000번을 열고, 요청을 받기 위해 대기 한다.
